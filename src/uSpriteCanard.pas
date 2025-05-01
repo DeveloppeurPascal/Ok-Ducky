5b3ba0552d00a22566e132b33d978689bd354df2
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-01 17:30:42 +0200

    added copyright and timestamp comments

--- src/uSpriteCanard.pas
+++ src/uSpriteCanard.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Ok Ducky!
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://okducky.gamolf.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Ok-Ducky
+///
+/// ***************************************************************************
+/// File last update : 2025-04-28T09:07:06.475+02:00
+/// Signature : 7081cae1fc097c359cbdf109d9292d41606d409d
+/// ***************************************************************************
+/// </summary>
+
 unit uSpriteCanard;
 
 interface
